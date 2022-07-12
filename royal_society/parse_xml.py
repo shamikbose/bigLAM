5e9e390b66abc64ac34480c76144859e40be4412
shamikbose <shamik.bose89@gmail.com>
2022-07-12 18:56:31 -0400

    Update parse_xml.py

--- royal_society/parse_xml.py
+++ royal_society/parse_xml.py
@@ -1,10 +1,26 @@
-from cgitb import text
-import xml.etree.ElementTree as et
+from xml.dom import minidom
 from bs4 import BeautifulSoup
 
+
+def is_tag(line):
+    if line.startswith("<") and line.endswith(">"):
+        return True
+    else:
+        return False
+
+
+def decode_tag(tag):
+    tag = BeautifulSoup(line, "lxml")
+    return tag
+
+
 filename = r"C:\Users\Shamik Bose\Downloads\Royal_Society_Corpus_v2.0.2_final\Royal_Society_Corpus_v2.0.2_final.vrt"
-with open(filename, "r") as f:
-    data = f.read()
+with open(filename, "r", encoding="utf-8") as f:
+    data = f.readlines()
+
 for line in data:
-    print(line)
-    input()
+    line = line.strip()
+    if is_tag(line):
+        tag = decode_tag(line)
+        print(tag.name)
+        input()
