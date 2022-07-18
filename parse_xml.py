25bb5ec9d4796d7c4c24b6f5f59fdc28da5b6f70
shamikbose <shamik.bose89@gmail.com>
2022-07-18 17:45:49 -0400

    Dataloader for lampeter_corpus

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,28 @@
-from cgitb import text
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-filename = r"C:\Users\Shamik Bose\Downloads\Royal_Society_Corpus_v2.0.2_final\Royal_Society_Corpus_v2.0.2_final.vrt"
-with open(filename, "r") as f:
-    data = f.read()
-bs_data = BeautifulSoup(data, "xml")
-b_text = bs_data.find_all("text")
-for text in b_text:
-    print(text.get("title"))
-    break
+filename = r"C:\Users\Shamik Bose\Downloads\3193.xml"
+with open(filename, "r", encoding="UTF-8") as f:
+    soup=BeautifulSoup(f, features='xml')
+    for entry in soup.find_all("TEI"):
+        text_parts = []
+        title_with_id = entry.teiHeader.fileDesc.titleStmt.title.text
+        id, title = title_with_id.split(":", maxsplit=1)
+        id = id.strip()
+        title=title.strip()
+        date=id[-4:]
+        content = entry.find("text")
+        head=content.find("body").find("head")
+        if head:
+            head=head.text
+        else:
+            head=""
+        body_parts=content.find("body").find_all("p")
+        for body_part in body_parts:
+            text_parts.append(body_part.text)
+        full_text = " ".join(text_parts)
+        print(f'ID:{id}, Date: {date}')
+        # input()
+
+
+
